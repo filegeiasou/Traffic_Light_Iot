60e1f0f87128b5fc04e60ab1560171107acc650a
Frosso Varsou <frossovarsou@gmail.com>
2024-10-24 02:28:26 +0300

    Update traffic_light.py (B part Done)

--- Raspberry_Pi/traffic_light.py
+++ Raspberry_Pi/traffic_light.py
@@ -2,10 +2,11 @@ from gpiozero import TrafficLights
 from time import sleep
 import requests
 
-key = "R1LCUDBNUIP9GOW3"
+key1 = "R1LCUDBNUIP9GOW3"
+key2 = "NNF1B9GGD4OMXA1M"
 
 def updateData(redStatus, greenStatus, amberStatus):
-    x = requests.get(f"https://api.thingspeak.com/update?api_key=" + key + "&field1=" + greenStatus + "&field2=" + amberStatus + "&field3=" + redStatus)
+    x = requests.get(f"https://api.thingspeak.com/update?api_key=" + key1 + "&field1=" + greenStatus + "&field2=" + amberStatus + "&field3=" + redStatus)
     if (x.status_code == 200):
         if (redStatus == "1"):
             print("Red Light: On")
@@ -17,9 +18,20 @@ def updateData(redStatus, greenStatus, amberStatus):
     else:
         print(f"Failed to update, Response: {x.status_code}")
 
+def updateField8(status, api_key):
+    x = requests.get(f"https://api.thingspeak.com/update?api_key=" + api_key + "&field8=" + status)
+    if (x.status_code == 200):
+        print(f"Field8: {status}")
+        print(F"Entry: "+ x.text)
+    else:
+        print(f"Failed to update, Response: {x.status_code}")
+
 if __name__ == "__main__":
     # Option 1: With TrafficLights
     lights = TrafficLights(7, 8, 25) # Red, Amber, Green
+    updateField8("0", key1)
+    updateField8("0", key2)
+    sleep(15)
     while True:
         # Red light on and off
         lights.red.on()
