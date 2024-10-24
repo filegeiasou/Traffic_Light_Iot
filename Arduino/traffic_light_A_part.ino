734dfc6c56032d35863f08c86341a9c490cfa09b
Frosso Varsou <frossovarsou@gmail.com>
2024-10-24 18:21:40 +0300

    Create traffic_light_A_part.ino

---
+++ Arduino/traffic_light_A_part.ino
@@ -0,0 +1,97 @@
+String ssid = "Simulator Wifi";
+String password = "";
+String host = "api.thingspeak.com";
+const int httpPort = 80;
+String api = "R1LCUDBNUIP9GOW3"; // Traffic Api Key
+
+// LED pin setup (adjust according to your pinout)
+int greenLED = 10; // Pin for green LED
+int orangeLED = 9; // Pin for orange LED
+int redLED = 8; // Pin for red LED
+
+int setupESP8266(void){
+  Serial.begin(115200);
+  Serial.println("AT");
+  delay(10);
+  if(!Serial.find("OK")) return 1;
+  
+  // Set WiFi mode to station
+  Serial.println("AT+CWMODE=1");
+  delay(2000);
+  if(!Serial.find("OK")) return 2;
+
+  // Connect to WiFi
+  String cmd = "AT+CWJAP=\"" + ssid + "\",\"" + password + "\"";
+  Serial.println(cmd);
+  delay(5000); // Wait a bit for the connection to establish
+  if(!Serial.find("OK")) return 3;
+
+  // Check IP address (optional, for verification)
+  Serial.println("AT+CIFSR");
+  delay(2000);
+  if(!Serial.find("OK")) return 4;
+
+  // Connect to ThingSpeak
+  Serial.println("AT+CIPSTART=\"TCP\",\"" + host + "\"," + httpPort);
+  delay(5000); // Wait for connection to establish
+  if(!Serial.find("OK")) return 5;
+
+  Serial.println("WiFi Connected and ThingSpeak API ready");
+  return 0; // Success
+}
+
+void setup() {
+  // Setup the ESP8266 and LEDs
+  pinMode(greenLED, OUTPUT);
+  pinMode(orangeLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+}
+
+// Thingspeak: Field1 = Green, Field2 = Orange, Field3 = Red
+void sendData(String green, String orange, String red) {
+
+  // Construct and send the HTTP GET request
+  String url = "/update?api_key=" + api + "&field1=" + green + "&field2=" + orange + "&field3=" + red;
+  String httpPacket = "GET " + url + " HTTP/1.1\r\nHost: " + String(host) + "\r\nConnection: close\r\n\r\n";
+  int length = httpPacket.length();
+  
+  Serial.print("AT+CIPSEND=");
+  Serial.println(length);
+  delay(10);
+  
+  Serial.print(httpPacket);
+  delay(10);
+  if(!Serial.find("SEND OK\r\n")) return;
+}
+
+void controlTrafficLight(String state) {
+  if(state == "red"){
+    digitalWrite(greenLED, LOW);
+    digitalWrite(orangeLED, LOW);
+    digitalWrite(redLED, HIGH);
+  } else if (state == "orange") {
+    digitalWrite(greenLED, LOW);
+    digitalWrite(orangeLED, HIGH);
+    digitalWrite(redLED, LOW);
+  } else if (state == "green") {
+    digitalWrite(greenLED, HIGH);
+    digitalWrite(orangeLED, LOW);
+    digitalWrite(redLED, LOW);
+  }
+  
+}
+
+void loop() {
+  // Simulate traffic light control
+  controlTrafficLight("red");
+  sendData("0", "0", "1");
+  delay(20000);
+
+  controlTrafficLight("green");
+  sendData("1", "0", "0");
+  delay(20000);
+
+  controlTrafficLight("orange");
+  sendData("0", "1", "0");
+  delay(15000);
+}
