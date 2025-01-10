b8a25a51e19e7a2b5b1b8dcdabd430c52798fb06
Jim <dimkon2003@gmail.com>
2025-01-11 01:11:34 +0200

    Updated part C logic

--- Arduino/traffic_light_C_part.ino
+++ Arduino/traffic_light_C_part.ino
@@ -16,16 +16,16 @@ int redLED = 13;
 unsigned long redTime = 30000;       // Red light duration time
 unsigned long greenTime = 30000;     // Green light duration time
 unsigned long orangeTime = 20000;    // Orange light duration time
-unsigned long alertDuration = 20000; // Alert mode duration
 
 // Variables
 unsigned long previousMillis = 0;        // Tracks last light change
 unsigned long alertStartTime = 0;        // Tracks start of alert mode
-unsigned long lastField8Update = 0;      // Last field8 update time
-unsigned long lastCheckMillis = 0;       // Last ThingSpeak check time
 unsigned long checkInterval = 5000;      // Check ThingSpeak Read Channel every 5 seconds
-unsigned long previousUpdateMillis = 0;  // Update interval (every 10 minutes)
-unsigned long updateInterval = 60000;    // Update interval (every 1 minute)
+unsigned long previousUpdateMillis = 0;
+unsigned long previousFieldCheckMillis = 0;
+unsigned long updateInterval = 600000;    // Update interval (every 10 minutes)
+unsigned long alertDuration = 60000;
+unsigned long previousAlertMillis = 0;  // To track time for alert mode (1 minute)
 int currentLight = 0;                    // 0 = Red, 1 = Green, 2 = Orange
 bool alertMode = false;                  // Alert mode state status
 
@@ -45,7 +45,7 @@ void setup() {
   }
   Serial.println("\nWiFi Connected");
 
-  setField8(0); // initially set field8 value to 0
+  setField8(0); // Initially set field8 value to 0
 }
 
 void controlTrafficLight(String state) {
@@ -76,8 +76,8 @@ int getField8() {
 
     if (httpResponseCode > 0) {
       String payload = http.getString();
-      Serial.print("Field 8 Value: ");
-      Serial.println(payload);
+      // Serial.print("Field 8 Value: ");
+      // Serial.println(payload);
       http.end();
       delay(100); // Delay to stabilize communication
       return payload.toInt();
@@ -91,6 +91,28 @@ int getField8() {
   return -1;
 }
 
+void sendData(String green, String orange, String red) {
+  String url = "http://api.thingspeak.com/update?api_key=" + api + "&field1=" + green + "&field2=" + orange + "&field3=" + red;
+  if (WiFi.status() == WL_CONNECTED) { // Check if connected to WiFi
+    HTTPClient http;
+    http.begin(url); // Specify the URL
+
+    int httpResponseCode = http.GET(); // Send the GET request
+
+    if (httpResponseCode > 0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    } else {
+      Serial.print("Error on sending request: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end(); // Free resources
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
+}
+
 void setField8(int value) {
   unsigned long currentMillis = millis();
 
@@ -102,8 +124,8 @@ void setField8(int value) {
     int httpResponseCode = http.GET();
 
     if (httpResponseCode > 0) {
-      Serial.print("Updated Field 8 to: ");
-      Serial.println(value);
+      // Serial.print("Updated Field 8 to: ");
+      // Serial.println(value);
       delay(500); // Allow time for the ThingSpeak channel to process the update
     } else {
       Serial.println("Error updating Field 8");
@@ -118,59 +140,69 @@ void setField8(int value) {
 void loop() {
   unsigned long currentMillis = millis();
 
-  // Periodically check ThingSpeak field
-  if (currentMillis - lastCheckMillis >= checkInterval) {
-    lastCheckMillis = currentMillis;
-    int fieldValue = getField8();
-    if (fieldValue == 1 && !alertMode) {
-      alertMode = true;
+  // // Update field8 every 10 minutes
+  // if (currentMillis - previousUpdateMillis >= updateInterval) {
+  //   previousUpdateMillis = currentMillis;
+
+  //   // Set field8 to 1 or 0 (depending on your logic)
+  //   setField8(1); // Or setField8(0) depending on the condition
+
+  //   Serial.println("Field 8 has been updated");
+  // }
+
+  // Check for keystroke to enter alert mode
+  if (Serial.available() > 0) {
+    char input = Serial.read();
+    if (input == 'e' && !alertMode) {
       alertStartTime = currentMillis; // Start alert mode
-      Serial.println("Entered Alert Mode via ThingSpeak!");
+      setField8(1); // Update field8 to 1
+      // delay(2000);
+      Serial.println("Entered Alert Mode via Keystroke!");
     }
   }
 
-  // Check if 10 minutes have passed in order to update Field 8 value
-  if (currentMillis - previousUpdateMillis >= updateInterval) {
-    previousUpdateMillis = currentMillis; // Reset the timer
-    setField8(1); // Change field 8 value to 1 to start alert mode
-    alertStartTime = currentMillis; // keep the time the alert mote started
-    alertMode = true; // start alert mode
-    Serial.println("Alert mode started.");
+  // Check field8 value at the specified interval (2 seconds)
+  if (currentMillis - previousFieldCheckMillis >= checkInterval) {
+    previousFieldCheckMillis = currentMillis;  // Update the last field check time
+
+    int field8Value = getField8();
+    Serial.print("Field 8 Value: ");
+    Serial.println(field8Value);
+
+    // If the field8 value is 1, enter alert mode
+    if (field8Value == 1) {
+      // alertMode = true;
+      previousAlertMillis = currentMillis;
+      Serial.println("Entered alert mode");
+      controlTrafficLight("orange");
+      delay(15000);
+      sendData("0", "1", "0");
+      delay(alertDuration);
+      Serial.println("Alert mode duration ended");
+      alertMode = false;
+      setField8(0);  // Reset field8 to 0 after alert mode ends
+      delay(15000);
+      return;  // Skip normal light cycle if in alert mode
+    }
   }
 
-  // // Check for keystroke to enter alert mode
-  // if (Serial.available() > 0) {
-  //   char input = Serial.read();
-  //   if (input == 'e' && !alertMode) {
-  //     alertMode = true;
-  //     alertStartTime = currentMillis; // Start alert mode
-  //     setField8(1); // Update field8 to 1
-  //     Serial.println("Entered Alert Mode via Keystroke!");
-  //   }
-  // }
-
-  // Handle alert mode
-  if (alertMode) {
-    controlTrafficLight("orange"); // turn orange light mode
-    delay(20000); // keep the orange light on for 20 seconds
-    alertMode = false; // Exit alert mode
-    setField8(0);      // Reset field8 value to 0
-    Serial.println("Exiting Alert Mode");
-    return; // Skip normal light cycle
-  }
-
-  // Normal light cycle
-  if (currentLight == 0 && currentMillis - previousMillis >= redTime) {
-    currentLight = 1; // Red to Green
-    previousMillis = currentMillis;
-    controlTrafficLight("red");
-  } else if (currentLight == 1 && currentMillis - previousMillis >= greenTime) {
-    currentLight = 2; // Green to Orange
-    previousMillis = currentMillis;
-    controlTrafficLight("green");
-  } else if (currentLight == 2 && currentMillis - previousMillis >= orangeTime) {
-    currentLight = 0; // Orange to Red
-    previousMillis = currentMillis;
-    controlTrafficLight("orange");
+  if(!alertMode) {
+    // Normal light cycle
+    if (currentLight == 0 && currentMillis - previousMillis >= redTime) {
+      currentLight = 1; // Red to Green
+      previousMillis = currentMillis;
+      controlTrafficLight("red");
+      sendData("0", "0", "1");
+    } else if (currentLight == 1 && currentMillis - previousMillis >= greenTime) {
+      currentLight = 2; // Green to Orange
+      previousMillis = currentMillis;
+      controlTrafficLight("green");
+      sendData("1", "0", "0");
+    } else if (currentLight == 2 && currentMillis - previousMillis >= orangeTime) {
+      currentLight = 0; // Orange to Red
+      previousMillis = currentMillis;
+      controlTrafficLight("orange");
+      sendData("0", "1", "0");
+    }
   }
 }
